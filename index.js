d1c137fe76c06bfd79f3a6fcbcd7f3ec05cc0f0e
Heather Kim <haesoo1108@gmail.com>
2019-02-11 22:01:32 +0900

    Tooltip shows Location name on hover

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+
+
+//tooltip displays
 var mouselocation;
 var tooltip = d3.select(".tooltip");
+var mappath = d3.selectAll(".map").selectAll("path");
 
-d3.select('body').on('mousemove', function(){
+mappath.on('mouseenter', function(){
+    tooltip.style('display', 'block');
+    let textID = this.id;
+    tooltip.select(".tooltip-city").html("<strong>"+textID+"</strong>");
+}).on('mousemove', function(){
     mouselocation = d3.mouse(d3.select('body').node());
-    tooltip.style('display', 'block')
-        .style("left", mouselocation[0]+110+"px")
+    tooltip.style("left", mouselocation[0]+120+"px")
         .style("top", mouselocation[1]+10+"px");
+}).on('mouseleave', function(){
+    tooltip.style('display', 'none');
 })
 
