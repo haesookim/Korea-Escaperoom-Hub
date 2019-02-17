8114851982f8a4b2bb01420709c87877a912819e
Heather Kim <haesoo1108@gmail.com>
2019-02-17 15:25:02 +0900

    editing heatmap functions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 //download the data
-var data = d3.csv("/data/ldhdata_clean.csv", function(room){
+var fulldata = d3.csv("/data/ldhdata_clean.csv", function(room){
     //clean it up a bit
     return{
         theme : room.themeName,
@@ -14,37 +14,43 @@ var data = d3.csv("/data/ldhdata_clean.csv", function(room){
     var count = d3.nest()
         .key(function(room){return room.spec_location; })
         .rollup(function(room_group){
-        return room_group.length;
+            return room_group.length;
         }).entries(data);
-    console.log(count);
+});
+    /*
+*/
+
+var seouldata = d3.csv("/data/seoul-data.csv", function(city){
+    return +city.number;
+})
+
+seouldata.then(function(){
+    console.log(seouldata);
 
     var colorScale = d3.scaleThreshold()
         .domain([10, 20, 30, 40, 50])
         .range(["#D3DEF8", "#acb8d8", "#8593b9", "#5e6d99", "#37487a", "#10235b"]);
-    });
 
-    /*var colorByCount = d3.selectAll(".map").selectAll("path").data(count)
-            .enter()
-            .select("#")
-            .attr('fill', function(d) { 
-                return colorScale(d.value); s
-        });
-*/
+    d3.select("#seoulmap").selectAll("path")
+        .data(seouldata)
+        .attr("id", function(d){return d});
+
+        console.log(d3.select("#seoulmap").selectAll("path"));
+})
 
 //tooltip displays
 var mouselocation;
 var tooltip = d3.select(".tooltip");
 var mappath = d3.selectAll(".map").selectAll("path");
 
-console.log(mappath);
-
 mappath.on('mouseenter', function(){
     tooltip.style('display', 'block');
     let textID = this.id;
     let locinfo;
     tooltip.select(".tooltip-city")
-        .html("<strong>"+textID+"</strong></br>")
-        .append()
+        .html("<strong>"+textID+"</strong></br>");
+    
+    tooltip.select(".tooltip-num")
         .html("# of Themes Visited: ");
 }).on('mousemove', function(){
     mouselocation = d3.mouse(d3.select('body').node());
